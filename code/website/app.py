ac68ea75d5d4e2314b21492f353d8c76b5838f51
AthiyaNahdhiana <140142411+AthiyaNahdhiana@users.noreply.github.com>
2024-07-29 10:55:30 +0700

    Update app.py

--- code/website/app.py
+++ code/website/app.py
@@ -5,13 +5,13 @@ import pickle
 #model 1 KNN
 model1_filename = 'C:/Users/athiy/Documents/1-KULIAH UDINUS/8 GENAP 23-24/Bengkel Koding/UAS/Heart-Disease-main/Heart-Disease-main/model/modelKNN.pkl'
 
-with open(model1_filename, 'rb') as file:
+with open(model1_filename, 'rb1') as file:
     model1 = pickle.load(file)
 
 #model 2 Random Forest
 model2_filename = 'C:/Users/athiy/Documents/1-KULIAH UDINUS/8 GENAP 23-24/Bengkel Koding/UAS/Heart-Disease-main/Heart-Disease-main/model/modelRandForest.pkl'
 
-with open(model2_filename, 'rb') as file:
+with open(model2_filename, 'rb2') as file:
     model2 = pickle.load(file)
 
 def main():
